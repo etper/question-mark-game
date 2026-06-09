53c33e28124d100ddfdc6164cdd2bc412d015976
etper <piotr.brzezinski9@gmail.com>
2026-06-09 18:06:59 +0200

    question appearing

--- main.gd
+++ main.gd
@@ -2,17 +2,28 @@ extends Node2D
 
 var paused_now = false
 
+var questions = [
+	"What is 2 + 2?",
+	"What color is the sky?"
+]
+
 func _ready():
-	$Timer.wait_time = 10.0
+	$QuestionPanel.visible = false
+
+	$Timer.wait_time = 1.0
 	$Timer.start()
 
 func _on_timer_timeout():
 	if paused_now:
 		get_tree().paused = false
+		$QuestionPanel.visible = false
 		$Timer.wait_time = 10.0
 	else:
 		get_tree().paused = true
-		$Timer.wait_time = 2.0
+
+		var q = questions.pick_random()
+		$QuestionPanel/QuestionLabel.text = q
+		$QuestionPanel.visible = true
 
 	paused_now = !paused_now
 	$Timer.start()
