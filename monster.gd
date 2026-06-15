304ca12ad567eb7ce6af031143e3a2b545bc9fe3
etper <piotr.brzezinski9@gmail.com>
2026-06-15 14:28:16 +0200

    e

--- monster.gd
+++ monster.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 const SPEED = 120.0
-var damage_cooldown = false
 
 @export var player: CharacterBody2D
+
+var can_damage = true
 
 func _physics_process(delta):
 	if player == null:
@@ -13,27 +14,6 @@ func _physics_process(delta):
 
 	velocity = direction * SPEED
 	move_and_slide()
-
-	if global_position.distance_to(player.global_position) < 40:
-		damage_player()
-
-func damage_player():
-	if damage_cooldown:
-		return
-
-	damage_cooldown = true
-
-	var main = get_tree().current_scene
-	main.lives -= 1
-	main.update_hearts()
-
-	if main.lives <= 0:
-		get_tree().change_scene_to_file("res://GameOver.tscn")
-
-	await get_tree().create_timer(1.0).timeout
-	damage_cooldown = false
-
-var can_damage = true
 
 func _on_area_2d_body_entered(body):
 	if body.name != "Player":
